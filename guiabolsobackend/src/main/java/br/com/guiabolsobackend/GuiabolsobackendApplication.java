355bc12d6ea3bd05f2c37aa431cdf51d9b70c0f7
Anderson Santo <kalangoti@gmail.com>
2020-09-23 00:51:34 -0300

    Project created

---
+++ guiabolsobackend/src/main/java/br/com/guiabolsobackend/GuiabolsobackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.guiabolsobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiabolsobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiabolsobackendApplication.class, args);
+	}
+
+}
---
+++ guiabolsobackend/src/test/java/br/com/guiabolsobackend/GuiabolsobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guiabolsobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiabolsobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
